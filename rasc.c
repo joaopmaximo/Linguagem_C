eb380a3f787417f0485f2c749ad6a9fea81deeb1
João Pedro Máximo <jp.maximoo@gmail.com>
2022-03-28 15:56:05 -0300

    att

---
+++ rasc.c
@@ -0,0 +1,18 @@
+// Fibonnaci
+
+#include <stdio.h>
+
+int main (void) {
+    int x, cont = 0, r, aux;
+
+    printf ("Insira um numero inteiro: ");
+    scanf ("%d", &x);
+
+    while (cont < x) {
+        if 
+        aux = 
+        cont++;
+    }
+
+    return 0;
+}
